862a0771de921a1aa07bc3f67e5435e1a256b5c1
Heo Dochi <dnjsakf@gmail.com>
2020-08-05 22:19:49 +0900

    set cors option

--- app/sockets/sockets.py
+++ app/sockets/sockets.py
@@ -3,7 +3,7 @@ import socketio
 from .namespaces import ChatNamespace
 
 def create_sio(**kwargs):
-  sio = socketio.Server(**kwargs)
+  sio = socketio.Server(**kwargs, cors_allowed_origins="*")
   sio.register_namespace(ChatNamespace(sio, '/chat'))
   
   return sio
